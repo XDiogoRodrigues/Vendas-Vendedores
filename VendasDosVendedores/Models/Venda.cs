2540e3d335c5e3ddc1d8ce589e4b7985d566633c
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-02-22 20:12:05 -0300

    Declarada a classe Venda com seus Annotations

---
+++ VendasDosVendedores/Models/Venda.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VendasDosVendedores.Models;
+
+public class Venda
+{
+    [Key]
+    public int VendaId { get; set; }
+
+    [Required]
+    [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
+    public DateTime Date { get; set; }
+    public Produto Produto { get; set; }
+    public int ProdutoId { get; set; }
+    [Required]
+    [Display(Name ="Quantidade do produto vendida")]
+    public int Quantidade { get; set; }
+
+    public Vendedor Vendedor { get; set; }
+    public int VendedorId { get; set; }
+}
