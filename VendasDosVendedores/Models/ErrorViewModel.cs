2adddff50de0550dbff175157f4fa4976588da7c
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-02-22 17:52:36 -0300

    Inicializado o projeto

---
+++ VendasDosVendedores/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VendasDosVendedores.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
