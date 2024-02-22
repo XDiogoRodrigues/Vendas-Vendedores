8bf98b319eb752414d0f1d0aed10f99081468a0c
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-02-22 18:35:20 -0300

    Declarada as classes Login e Vendedor com seus atributos e Annotations

---
+++ VendasDosVendedores/Models/Login.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VendasDosVendedores.Models;
+
+public class Login
+{
+    [Required]
+    [EmailAddress]
+    public string Usuario { get; set; }
+
+    [Required]
+    [RegularExpression("^(?=.*[A-Z])(?=.*[!#@$%&])(?=.*[0-9])(?=.*[a-z]).{6,15}$", ErrorMessage ="A Senha deve ter no mínimo 6 caracteres e no máximo 15, uma letra maiúscula e minúscula e um número, e um caractere especial(!#@$%&)")]
+    public string Senha { get; set; }
+
+    public int VendedorId { get; set; }
+    public virtual Vendedor Vendedor { get; set; }
+}
---
+++ VendasDosVendedores/Models/Vendedor.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VendasDosVendedores.Models;
+
+public class Vendedor
+{
+    [Required]
+    [StringLength(200, MinimumLength =3, ErrorMessage ="O {} deve ter no mínimo {} palavras e no máximo {}")]
+    [Display(Name = "Nome do vendedor")]
+    public string Nome { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [Display(Name ="Email do vendedor")]
+    public string Email { get; set; }
+
+    [Required]
+    [Display(Name ="Rg do vendedor")]
+    [RegularExpression("(^\\d{1,2}).?(\\d{3}).?(\\d{3})-?(\\d{1,2}|X|x$)", ErrorMessage ="O Rg deve ser escrito dessa forma: xx.xxx.xxx-xx")]
+    public string RG { get; set; }
+
+    [Required]
+    [Display(Name = "Cpf do vendedor")]
+    [RegularExpression("(^\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}$)")]
+    public string Cpf { get; set; }
+
+    [Required]
+    [Display(Name = "Endereço do vendedor")]
+    public string Endereco { get; set; }
+}
